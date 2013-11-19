7559e04060f57c341fc83c91758e78a2c1502565
Law  Michael Dennis <lawmich3@ug168.eecg>
2013-11-19 00:48:43 -0500

    3 - parallelizing with pthreads

---
+++ hw4/mrun.sh
@@ -0,0 +1,4 @@
+rm randtrack.cc
+wget https://dl.dropboxusercontent.com/u/19920404/ece454/hw4/randtrack.cc
+make clean
+make
--- hw4/randtrack.cc
+++ hw4/randtrack.cc
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "defs.h"
 #include "hash.h"
@@ -41,26 +42,25 @@ class sample {
   void print(FILE *f){printf("%d %d\n",my_key,count);}
 };
 
+class param {
+  public:
+    int start;
+    int end;
+};
+
 // This instantiates an empty hash table
 // it is a C++ template, which means we define the types for
 // the element and key value here: element is "class sample" and
 // key value is "unsigned".  
 hash<sample,unsigned> h;
 
+void * process_stream (void *ptr);
 
 
-void * print_message_function(void *ptr);
-
-
-
-int  
-main (int argc, char* argv[]){
-  int i,j,k;
-  int rnum;
-  unsigned key;
-  sample *s;
+int main (int argc, char* argv[]){
 
   // Print out team information
+  /*
   printf( "Team Name: %s\n", team.team );
   printf( "\n" );
   printf( "Student 1 Name: %s\n", team.name1 );
@@ -71,6 +71,7 @@ main (int argc, char* argv[]){
   printf( "Student 2 Student Number: %s\n", team.number2 );
   printf( "Student 2 Email: %s\n", team.email2 );
   printf( "\n" );
+  */
 
   // Parse program arguments
   if (argc != 3){
@@ -83,8 +84,58 @@ main (int argc, char* argv[]){
   // initialize a 16K-entry (2**14) hash of empty lists
   h.setup(14);
 
+  int start = 0;
+  int i, end, incr;
+
+  switch (num_threads) {
+    case 1:
+      end = 4;
+      incr = 0;
+      break;
+    case 2:
+      end = 2;
+      incr = 2;
+      break;
+    case 4:
+      end = 1;
+      incr = 1;
+      break;
+    default:
+      return 0;
+      break;
+  }
+
+  pthread_t thread[num_threads];
+  param *p = new param[num_threads];
+
+  for (i=0; i< num_threads; i++){
+    p[i].start = start;
+    p[i].end = end;
+    pthread_create(&thread[i], NULL, process_stream, (void*) &p[i]);
+    start+=incr;
+    end+=incr;
+
+  }
+  for (i=0; i<num_threads; i++){
+    pthread_join(thread[i],NULL);
+  }
+
+  // print a list of the frequency of all samples
+  h.print();
+
+
+}
+
+
+void * process_stream (void *ptr) {
+
+  param *p = (param*) ptr;
+
+  int i,j,k;
+  int rnum, key;
+  sample * s;
   // process streams starting with different initial numbers
-  for (i=0; i<NUM_SEED_STREAMS; i++){
+  for (i=p->start; i<p->end; i++){
     rnum = i;
 
     // collect a number of samples
@@ -100,43 +151,16 @@ main (int argc, char* argv[]){
 
       // if this sample has not been counted before
       if (!(s = h.lookup(key))){
-      	// insert a new element for it into the hash table
-      	s = new sample(key);
-      	h.insert(s);
+        // insert a new element for it into the hash table
+        s = new sample(key);
+        h.insert(s);
       }
 
       // increment the count for the sample
       s->count++;
     }
   }
-  // print a list of the frequency of all samples
-  h.print();
 
-
-
-
-  //try out some pthreads here
-  pthread_t thread1, thread2;
-  const char *message1 = "Thread 1";
-  const char *message2 = "Thread 2";
-  int  iret1, iret2;
-
-
-  iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
-  iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
-
-  pthread_join(thread1, NULL);
-  pthread_join(thread2, NULL);
-
-  printf("Thread 1 returns: %d\n",iret1);
-  printf("Thread 2 returns: %d\n",iret2);
-}
-
-
-void * print_message_function(void *ptr) {
-  char * message;
-  message = (char *) ptr;
-  printf("%s \n", message);
 }
 
 
