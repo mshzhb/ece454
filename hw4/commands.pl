ce6651327a23c472fba7249d23625d24e8ec0f29
lawmicha <mlaw135@gmail.com>
2013-11-21 12:17:57 -0500

    commands

--- hw4/commands.pl
+++ hw4/commands.pl
@@ -10,11 +10,20 @@ for ($i = 0; $i < $MAX_RUNS; $i++){
 	system($cmd);
 }
 
+my $cmd = "/usr/bin/time -f 'randtrack_1_100 %e' -a -o ". $output." ./randtrack 1 100";
+print ($cmd);
+
+my $MAX_RUNS = 5;
+for ($i = 0; $i < $MAX_RUNS; $i++){
+	print("run ".$i." of ".$MAX_RUNS."\n");
+	system($cmd);
+}
+
 my $samples = 50;
 while ($samples < 101) {
 	my $counter = 1;
 	while ($counter < 5){
-		$cmd = "/usr/bin/time -f 'gl_1_50 %e' -a -o ". $output." ./randtrack_global_lock ".$counter." ".$samples." ";
+		$cmd = "/usr/bin/time -f 'gl_".$counter."_".$samples." %e' -a -o ". $output." ./randtrack_global_lock ".$counter." ".$samples." ";
 		print ($cmd);
 		my $MAX_RUNS = 5;
 		for ($i = 0; $i < $MAX_RUNS; $i++){
@@ -22,7 +31,7 @@ while ($samples < 101) {
 			system($cmd);
 		}
 
-		$cmd = "/usr/bin/time -f 'tm_1_50 %e' -a -o ". $output." ./randtrack_tm ".$counter." ".$samples." ";
+		$cmd = "/usr/bin/time -f 'tm_".$counter."_".$samples." %e' -a -o ". $output." ./randtrack_tm ".$counter." ".$samples." ";
 		print ($cmd);
 		my $MAX_RUNS = 5;
 		for ($i = 0; $i < $MAX_RUNS; $i++){
@@ -30,7 +39,7 @@ while ($samples < 101) {
 			system($cmd);
 		}
 
-		$cmd = "/usr/bin/time -f 'll_1_50 %e' -a -o ". $output." ./randtrack_list_lock ".$counter." ".$samples." ";
+		$cmd = "/usr/bin/time -f 'll_".$counter."_".$samples." %e' -a -o ". $output." ./randtrack_list_lock ".$counter." ".$samples." ";
 		print ($cmd);
 		my $MAX_RUNS = 5;
 		for ($i = 0; $i < $MAX_RUNS; $i++){
@@ -38,7 +47,7 @@ while ($samples < 101) {
 			system($cmd);
 		}
 
-		$cmd = "/usr/bin/time -f 'el_1_50 %e' -a -o ". $output." ./randtrack_element_lock ".$counter." ".$samples." ";
+		$cmd = "/usr/bin/time -f 'el_".$counter."_".$samples." %e' -a -o ". $output." ./randtrack_element_lock ".$counter." ".$samples." ";
 		print ($cmd);
 		my $MAX_RUNS = 5;
 		for ($i = 0; $i < $MAX_RUNS; $i++){
