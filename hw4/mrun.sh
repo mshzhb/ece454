be39759921d5da5c13ffc945a1f9d425dd48dbe8
Law  Michael Dennis <lawmich3@ug168.eecg>
2013-11-21 16:18:26 -0500

    submitted

--- hw4/mrun.sh
+++ hw4/mrun.sh
@@ -1,6 +1,4 @@
-rm hash_element_lock.h
-wget https://dl.dropboxusercontent.com/u/19920404/ece454/hw4/hash_element_lock.h
-rm randtrack_element_lock.cc
-wget https://dl.dropboxusercontent.com/u/19920404/ece454/hw4/randtrack_element_lock.cc
+rm randtrack_reduction.cc
+wget https://dl.dropboxusercontent.com/u/19920404/ece454/hw4/randtrack_reduction.cc
 make clean
 make
