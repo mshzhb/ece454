aade8371de12576608858a32c41a46d1c56ddcc0
lawmicha <mlaw135@gmail.com>
2013-11-20 23:15:55 -0500

    measurements

--- hw4/commands.pl
+++ hw4/commands.pl
@@ -1,14 +1,12 @@
 my $output = $ARGV[0];
 system("touch " . $output);
 
-#my $cmd = "/usr/bin/time -a -o ". $output." vpr iir1.map4.latren.net k4-n10.xml place.out route.out -nodisp -place_only -seed 0";
-my $cmd = "/usr/bin/time -a -o ". $output." make";
+my $cmd = "/usr/bin/time -a -o ". $output." ./randtrack 0 50";
 print ($cmd);
 
 my $MAX_RUNS = 5;
 for ($i = 0; $i < $MAX_RUNS; $i++){
 	print("run ".$i." of ".$MAX_RUNS."\n");
-	system("make clean");
 	system($cmd);
 }
 
