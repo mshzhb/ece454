f1f18553795ce1e779e3d9e51df45a275043a89c
lawmicha <mlaw135@gmail.com>
2013-11-21 11:54:51 -0500

    comands

--- hw4/commands.pl
+++ hw4/commands.pl
@@ -10,87 +10,50 @@ for ($i = 0; $i < $MAX_RUNS; $i++){
 	system($cmd);
 }
 
-$cmd = "/usr/bin/time -f 'gl_1_50 %e' -a -o ". $output." ./randtrack_global_lock 1 50";
-print ($cmd);
+my $samples = 50;
+while ($samples < 101) {
+	my $counter = 1;
+	while (counter < 5){
+		$cmd = "/usr/bin/time -f 'gl_1_50 %e' -a -o ". $output." ./randtrack_global_lock ".$counter." ".$samples." ";
+		print ($cmd);
 
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
+		my $MAX_RUNS = 5;
+		for ($i = 0; $i < $MAX_RUNS; $i++){
+			print("run ".$i." of ".$MAX_RUNS."\n");
+			system($cmd);
+		}
+
+		$cmd = "/usr/bin/time -f 'tm_1_50 %e' -a -o ". $output." ./randtrack_tm ".$counter." ".$samples." ";
+		print ($cmd);
+
+		my $MAX_RUNS = 5;
+		for ($i = 0; $i < $MAX_RUNS; $i++){
+			print("run ".$i." of ".$MAX_RUNS."\n");
+			system($cmd);
+		}
+
+		$cmd = "/usr/bin/time -f 'll_1_50 %e' -a -o ". $output." ./randtrack_list_lock ".$counter." ".$samples." ";
+		print ($cmd);
+
+		my $MAX_RUNS = 5;
+		for ($i = 0; $i < $MAX_RUNS; $i++){
+			print("run ".$i." of ".$MAX_RUNS."\n");
+			system($cmd);
+		}
+
+		$cmd = "/usr/bin/time -f 'el_1_50 %e' -a -o ". $output." ./randtrack_element_lock ".$counter." ".$samples." ";
+		print ($cmd);
+
+		my $MAX_RUNS = 5;
+		for ($i = 0; $i < $MAX_RUNS; $i++){
+			print("run ".$i." of ".$MAX_RUNS."\n");
+			system($cmd);
+		}
+		$counter = $counter + $counter;
+	}
+	$samples = $samples + $samples;
 }
 
-$cmd = "/usr/bin/time -f 'tm_1_50 %e' -a -o ". $output." ./randtrack_tm 1 50";
-print ($cmd);
-
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
-}
-
-$cmd = "/usr/bin/time -f 'll_1_50 %e' -a -o ". $output." ./randtrack_list_lock 1 50";
-print ($cmd);
-
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
-}
-
-$cmd = "/usr/bin/time -f 'el_1_50 %e' -a -o ". $output." ./randtrack_element_lock 1 50";
-print ($cmd);
-
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
-}
 
 
 
-
-
-my $cmd = "/usr/bin/time -f 'randtrack_1_100 %e' -a -o ". $output." ./randtrack 1 100";
-print ($cmd);
-
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
-}
-
-$cmd = "/usr/bin/time -f 'gl_1_100 %e' -a -o ". $output." ./randtrack_global_lock 1 100";
-print ($cmd);
-
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
-}
-
-$cmd = "/usr/bin/time -f 'tm_1_100 %e' -a -o ". $output." ./randtrack_tm 1 100";
-print ($cmd);
-
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
-}
-
-$cmd = "/usr/bin/time -f 'll_1_100 %e' -a -o ". $output." ./randtrack_list_lock 1 100";
-print ($cmd);
-
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
-}
-
-$cmd = "/usr/bin/time -f 'el_1_100 %e' -a -o ". $output." ./randtrack_element_lock 1 100";
-print ($cmd);
-
-my $MAX_RUNS = 5;
-for ($i = 0; $i < $MAX_RUNS; $i++){
-	print("run ".$i." of ".$MAX_RUNS."\n");
-	system($cmd);
-}
