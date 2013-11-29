916ac7fffff5310907561064daff99ba9e100391
lawmicha <mlaw135@gmail.com>
2013-11-29 18:54:41 -0500

    neighbouring with 1 thread

--- hw5/lifeseq.c
+++ hw5/lifeseq.c
@@ -21,7 +21,6 @@
 
 #define BOARD( __board, __i, __j )  (__board[(__i) + LDA*(__j)])
 
-
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
 #define IS_ALIVE(var) ((var) & (1<<(4))) //check if the 5th bit is set --> alive
@@ -39,9 +38,8 @@
 
 #define INCR_AT(__board, __i, __j )  (__board[(__i) + nrows*(__j)] ++ )
 #define DECR_AT(__board, __i, __j )  (__board[(__i) + nrows*(__j)] -- )
-
-
-
+#define COUNT_OF_BOARD(__board, __i, __j )  (__board[(__i) + nrows*(__j)] & 0x0f  )
+#define TOP_4_BITS(__board, __i, __j )  (__board[(__i) + nrows*(__j)] & 0xf0  )
 
 void incr_neighbours2(char * board, int index, int nrows, int ncols);
 void * process (void *ptr);
@@ -63,73 +61,10 @@ sequential_game_of_life (char* outboard,
         const int ncols,
         const int gens_max)
 {
-    /* HINT: in the parallel decomposition, LDA may not be equal to
-       nrows! */
+
     const int LDA = nrows;
     int curgen, i, j;
 
-    /*
-    for (curgen = 0; curgen < gens_max; curgen++)
-    {
-
-        for (i = 0; i < nrows; i++)
-        {
-            for (j = 0; j < ncols; j++)
-            {
-                const int inorth = mod (i-1, nrows);
-                const int isouth = mod (i+1, nrows);
-                const int jwest = mod (j-1, ncols);
-                const int jeast = mod (j+1, ncols);
-
-                const char neighbor_count = 
-                    BOARD (inboard, inorth, jwest) + 
-                    BOARD (inboard, inorth, j) + 
-                    BOARD (inboard, inorth, jeast) + 
-                    BOARD (inboard, i, jwest) +
-                    BOARD (inboard, i, jeast) + 
-                    BOARD (inboard, isouth, jwest) +
-                    BOARD (inboard, isouth, j) + 
-                    BOARD (inboard, isouth, jeast);
-
-                BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
-
-            }
-        }
-        SWAP_BOARDS( outboard, inboard );
-
-    }
-    */
-    /* 
-     * We return the output board, so that we know which one contains
-     * the final result (because we've been swapping boards around).
-     * Just be careful when you free() the two boards, so that you don't
-     * free the same one twice!!! 
-     */
-	
-    //first, set up the neighbour counts within the inboard and outboard    
-
-    printf("\n---BEGIN\n");
-    printf("\n---INBOARD\n");
-
-    for (i = 0; i < nrows; i++)
-    {
-        for (j = 0; j < ncols; j++)
-        {
-            char cell = BOARD(inboard,i,j);
-            printf("\nat %x ",cell);
-        }
-    }
-    printf("\n---BEGIN\n");
-    printf("\n---OUTBOARD\n");
-
-    for (i = 0; i < nrows; i++)
-    {
-        for (j = 0; j < ncols; j++)
-        {
-            char cell = BOARD(outboard,i,j);
-            printf("\nat %x ",cell);
-        }
-    }
 
     int num_threads = 1;
     pthread_t thread[num_threads];
@@ -156,32 +91,35 @@ sequential_game_of_life (char* outboard,
     
     for (curgen = 0; curgen < gens_max; curgen++)
     {
-         printf("\n---CURGEN\n");
+        /*
+        printf("\n---CURGEN %d\n",curgen);
         printf("\n---INBOARD\n");
-
         for (i = 0; i < nrows; i++)
         {
             for (j = 0; j < ncols; j++)
             {
+                SET_CLEAN(BOARD(inboard,i,j));
                 char cell = BOARD(inboard,i,j);
                 printf("\nat %x ",cell);
             }
         }
-        printf("\n---CURGEN\n");
         printf("\n---OUTBOARD\n");
-
         for (i = 0; i < nrows; i++)
         {
             for (j = 0; j < ncols; j++)
             {
+
                 char cell = BOARD(outboard,i,j);
                 printf("\nat %x ",cell);
             }
+
         }
+        */
+
         for (i=0; i<num_threads; i++){
             ptr[i].inboard = inboard;
             ptr[i].outboard = outboard;
-            
+            //printf("\n----RUN! %d\n",i);
             pthread_create(&thread[i], NULL, process, (void*) &ptr[i]);
         }
 
@@ -194,13 +132,37 @@ sequential_game_of_life (char* outboard,
 
     }
 
+/*
     printf("\n---COMPLETE\n");
+    printf("\n---INBOARD\n");
     for (i = 0; i < nrows; i++)
     {
         for (j = 0; j < ncols; j++)
         {
+            SET_CLEAN(BOARD(inboard,i,j));
             char cell = BOARD(inboard,i,j);
             printf("\nat %x ",cell);
+        }
+    }
+    printf("\n---OUTBOARD\n");
+    for (i = 0; i < nrows; i++)
+    {
+        for (j = 0; j < ncols; j++)
+        {
+            SET_CLEAN(BOARD(outboard,i,j));
+            char cell = BOARD(outboard,i,j);
+            printf("\nat %x ",cell);
+        }
+    }
+*/
+
+    //todo: fix output, shift all to the right >> 4
+    for (i = 0; i < nrows; i++)
+    {
+        for (j = 0; j < ncols; j++)
+        {
+            SET_CLEAN(BOARD(inboard,i,j));
+            BOARD(inboard,i,j) = BOARD(inboard,i,j) >> 4;
         }
     }
 
@@ -223,72 +185,51 @@ void * process (void *ptr) {
     int j;
     int LDA = p->LDA;
     
-    printf("\n----PROCESS\n");
-    /* HINT: you'll be parallelizing these loop(s) by doing a
-       geometric decomposition of the output */
+  
     for (i = start_row; i < end_row; i++)
     {
         for (j = 0; j < ncols; j++)
         {
-            /*
-            const int inorth = mod (i-1, nrows);
-            const int isouth = mod (i+1, nrows);
-            const int jwest = mod (j-1, ncols);
-            const int jeast = mod (j+1, ncols);
-
-            const char neighbor_count = 
-                BOARD (inboard, inorth, jwest) + 
-                BOARD (inboard, inorth, j) + 
-                BOARD (inboard, inorth, jeast) + 
-                BOARD (inboard, i, jwest) +
-                BOARD (inboard, i, jeast) + 
-                BOARD (inboard, isouth, jwest) +
-                BOARD (inboard, isouth, j) + 
-                BOARD (inboard, isouth, jeast);
-
-            BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
-            */
-            //printf("%x\n",inboard[i+end_row*j]);
-
             //reset clean bit for inboard for next iteration
             SET_CLEAN(BOARD(inboard,i,j));
 
             char cell = BOARD(inboard,i,j);
             int alive = cell >> 4;
-            printf("\nat %x with state %d ",cell, alive);
+            //printf("\nat %x with state %d ",cell, alive);
             if (!alive) {
                 if (cell == 0x03) {
-                    printf("YEA! going to be alive!");
+                    //printf("YEA! going to be alive!");
                     const int inorth = mod (i-1, nrows);
                     const int isouth = mod (i+1, nrows);
                     const int jwest = mod (j-1, ncols);
                     const int jeast = mod (j+1, ncols);
 
                     if (IS_CLEAN(BOARD (outboard, inorth, jwest)))
-                        BOARD (outboard, inorth, jwest) = BOARD (inboard, inorth, jwest);
+                        BOARD(outboard,inorth, jwest) = TOP_4_BITS(outboard, inorth, jwest) | COUNT_OF_BOARD(inboard, inorth, jwest);
 
                     if (IS_CLEAN(BOARD (outboard, inorth, j)))
-                        BOARD (outboard, inorth, j) = BOARD (inboard, inorth, j);
+                        BOARD(outboard,inorth, j) = TOP_4_BITS(outboard, inorth, j) | COUNT_OF_BOARD(inboard, inorth, j);
 
                     if (IS_CLEAN(BOARD (outboard, inorth, jeast)))
-                        BOARD (outboard, inorth, jeast) = BOARD (inboard, inorth, jeast);
+                        BOARD(outboard,inorth, jeast) = TOP_4_BITS(outboard, inorth, jeast) | COUNT_OF_BOARD(inboard, inorth, jeast);
 
                     if (IS_CLEAN(BOARD (outboard, i, jwest)))
-                        BOARD (outboard, i, jwest) = BOARD (inboard, i, jwest);
+                        BOARD(outboard,i, jwest) = TOP_4_BITS(outboard, i, jwest) | COUNT_OF_BOARD(inboard, i, jwest);
 
                     if (IS_CLEAN(BOARD (outboard, i, jeast)))
-                        BOARD (outboard, i, jeast) = BOARD (inboard, i, jeast);
+                        BOARD(outboard,i, jeast) = TOP_4_BITS(outboard, i, jeast) | COUNT_OF_BOARD(inboard, i, jeast);
 
                     if (IS_CLEAN(BOARD (outboard, isouth, jwest)))
-                        BOARD (outboard, isouth, jwest) = BOARD (inboard, isouth, jwest);
+                        BOARD(outboard,isouth, jwest) = TOP_4_BITS(outboard, isouth, jwest) | COUNT_OF_BOARD(inboard, isouth, jwest);
 
                     if (IS_CLEAN(BOARD (outboard, isouth, j)))
-                        BOARD (outboard, isouth, j) = BOARD (inboard, isouth, j);
+                        BOARD(outboard, isouth, j) = TOP_4_BITS(outboard, isouth, j) | COUNT_OF_BOARD(inboard, isouth, j);
 
                     if (IS_CLEAN(BOARD (outboard, isouth, jeast)))
-                        BOARD (outboard, isouth, jeast) = BOARD (inboard, isouth, jeast);
+                        BOARD(outboard,isouth, jeast) = TOP_4_BITS(outboard, isouth, jeast) | COUNT_OF_BOARD(inboard, isouth, jeast);
 
                     SET_ALIVE(BOARD(outboard,i,j));
+                    SET_ALIVE(BOARD(inboard,i,j));
 
                     INCR_AT (outboard, inorth, jwest);
                     INCR_AT (outboard, inorth, j);
@@ -311,37 +252,38 @@ void * process (void *ptr) {
                 }
             } else if (alive) {
                 if (cell <= 0x11 || cell >=0x14) {
-                    printf("FOUND A LIFE GONNA DIE");
+                    //printf("FOUND A LIFE GONNA DIE");
                     const int inorth = mod (i-1, nrows);
                     const int isouth = mod (i+1, nrows);
                     const int jwest = mod (j-1, ncols);
                     const int jeast = mod (j+1, ncols);
 
                     if (IS_CLEAN(BOARD (outboard, inorth, jwest)))
-                        BOARD (outboard, inorth, jwest) = BOARD (inboard, inorth, jwest);
+                        BOARD(outboard,inorth, jwest) = TOP_4_BITS(outboard, inorth, jwest) | COUNT_OF_BOARD(inboard, inorth, jwest);
 
                     if (IS_CLEAN(BOARD (outboard, inorth, j)))
-                        BOARD (outboard, inorth, j) = BOARD (inboard, inorth, j);
+                        BOARD(outboard,inorth, j) = TOP_4_BITS(outboard, inorth, j) | COUNT_OF_BOARD(inboard, inorth, j);
 
                     if (IS_CLEAN(BOARD (outboard, inorth, jeast)))
-                        BOARD (outboard, inorth, jeast) = BOARD (inboard, inorth, jeast);
+                        BOARD(outboard,inorth, jeast) = TOP_4_BITS(outboard, inorth, jeast) | COUNT_OF_BOARD(inboard, inorth, jeast);
 
                     if (IS_CLEAN(BOARD (outboard, i, jwest)))
-                        BOARD (outboard, i, jwest) = BOARD (inboard, i, jwest);
+                        BOARD(outboard,i, jwest) = TOP_4_BITS(outboard, i, jwest) | COUNT_OF_BOARD(inboard, i, jwest);
 
                     if (IS_CLEAN(BOARD (outboard, i, jeast)))
-                        BOARD (outboard, i, jeast) = BOARD (inboard, i, jeast);
+                        BOARD(outboard,i, jeast) = TOP_4_BITS(outboard, i, jeast) | COUNT_OF_BOARD(inboard, i, jeast);
 
                     if (IS_CLEAN(BOARD (outboard, isouth, jwest)))
-                        BOARD (outboard, isouth, jwest) = BOARD (inboard, isouth, jwest);
+                        BOARD(outboard,isouth, jwest) = TOP_4_BITS(outboard, isouth, jwest) | COUNT_OF_BOARD(inboard, isouth, jwest);
 
                     if (IS_CLEAN(BOARD (outboard, isouth, j)))
-                        BOARD (outboard, isouth, j) = BOARD (inboard, isouth, j);
+                        BOARD(outboard, isouth, j) = TOP_4_BITS(outboard, isouth, j) | COUNT_OF_BOARD(inboard, isouth, j);
 
                     if (IS_CLEAN(BOARD (outboard, isouth, jeast)))
-                        BOARD (outboard, isouth, jeast) = BOARD (inboard, isouth, jeast);
+                        BOARD(outboard,isouth, jeast) = TOP_4_BITS(outboard, isouth, jeast) | COUNT_OF_BOARD(inboard, isouth, jeast);
 
                     SET_DEAD(BOARD(outboard,i,j));
+                    SET_DEAD(BOARD(inboard,i,j));
 
                     DECR_AT (outboard, inorth, jwest);
                     DECR_AT (outboard, inorth, j);
@@ -364,6 +306,12 @@ void * process (void *ptr) {
 
                 }
             }
+
+
+            if (IS_CLEAN(BOARD (outboard, i, j))) 
+                BOARD(outboard,i, j) = TOP_4_BITS(inboard, i, j) | COUNT_OF_BOARD(inboard, i, j);
+            SET_DIRTY (BOARD(outboard, i, j));
+
 
 
 
